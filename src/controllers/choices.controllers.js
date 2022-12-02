4f560909ea46984b998f2dd63c4f5f584a3dc38e
mcrestani <marianascrestani@gmail.com>
2022-12-02 17:54:33 -0300

    feat: post /choice

--- src/controllers/choices.controllers.js
+++ src/controllers/choices.controllers.js
@@ -1,16 +1,15 @@
+import { ObjectId } from "mongodb";
 import { choicesCollection } from "../database/db.js";
 
 export async function registerNewChoice(req, res) {
-  const title = res.locals.title;
-  const pollId = res.locals.pollId;
+  const { title, pollId } = req.body;
 
   try {
-    await choicesCollection.insertOne({ title, pollId });
-    res.send({ title, pollId }).status(201);
+    await choicesCollection.insertOne({ title, pollId: ObjectId(pollId) });
+    const newChoice = await choicesCollection.findOne({ title });
+    res.send({ newChoice }).status(201);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
   }
 }
-
-
--- src/routes/choices.routes.js
+++ src/routes/choices.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { choiceValidation } from "../middlewares/choices.middlewares.js";
+//import { choiceValidation } from "../middlewares/choices.middlewares.js";
 import { registerNewChoice } from "../controllers/choices.controllers.js";
 
 const choicesRouter = Router();
 
-choicesRouter.post("/choice", choiceValidation, registerNewChoice);
+//choicesRouter.post("/choice", choiceValidation, registerNewChoice);
+choicesRouter.post("/choice", registerNewChoice);
 
 export default choicesRouter;
