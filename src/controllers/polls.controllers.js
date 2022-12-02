6e1e1c00a3c07a32f77afcca6493e57e7758b89d
mcrestani <marianascrestani@gmail.com>
2022-12-02 17:02:42 -0300

    feat: post /polls com config expireAt

--- src/controllers/polls.controllers.js
+++ src/controllers/polls.controllers.js
@@ -1,14 +1,13 @@
 import { pollsCollection } from "../database/db.js";
 
 export async function registerNewPoll(req, res) {
-  //const poll = req.body;
-  const { title, expireAt } = req.body;
-  console.log("title e expire no controller", title, expireAt);
+  const expireAt = res.locals.expireAt;
+  const title = res.locals.title;
 
   try {
     await pollsCollection.insertOne({ title, expireAt });
-    console.log("ver se chega aqui");
-    res.send({ title, expireAt, _id }).status(201);
+    const newPoll = await pollsCollection.findOne({ title });
+    res.send({ newPoll }).status(201);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
@@ -18,7 +17,6 @@ export async function registerNewPoll(req, res) {
 export async function returnPolls(req, res) {
   try {
     const polls = await pollsCollection.find({}).toArray();
-    console.log("polls", polls);
     res.send(polls);
   } catch (err) {
     console.log(err);
--- src/middlewares/polls.middlewares.js
+++ src/middlewares/polls.middlewares.js
@@ -2,15 +2,18 @@ import dayjs from "dayjs";
 
 export function registerPollValidation(req, res, next) {
   const { title, expireAt } = req.body;
+  let newExpireAt = expireAt;
 
   if (!title) {
     return res.status(422).send({ message: error.detail || error.message });
   }
 
   if (!expireAt) {
-    const newExpireAt = dayjs().format("YYYY-MM-DD hh:mm").add(30, "day");
-    expireAt = newExpireAt;
+    const addedDaysToExpire = dayjs().add(30, "day");
+    newExpireAt = addedDaysToExpire.format("YYYY-MM-DD hh:mm");
   }
 
+  res.locals.expireAt = newExpireAt;
+  res.locals.title = title;
   next();
 }
